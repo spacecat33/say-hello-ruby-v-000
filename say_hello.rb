61a290b767079c0da5bd74feb5965f122deff5b6
Janan Akkad <jananakkad@hotmail.co.uk>
2019-01-07 21:23:00 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
-def say_hello
+def say_hello(name = "Ruby Programmer!")
 puts "Hello #{name}"
 end
 say_hello ("Gabriela")
 say_hello ("name")
 name = "Ruby Programmer!"
-say_hello = ("name") 
+say_hello = ("name")
