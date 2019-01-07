a76b701e35430b0b37c747bcb20e0db7d087aa28
Janan Akkad <jananakkad@hotmail.co.uk>
2019-01-07 21:42:23 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 say_hello("Gabriela")
 def say_hello(name = "Ruby Programmer!")
-puts "Hello #{name}"
+puts "Hello #{name!}"
 end
-say_hello("Kent Beck!")
+say_hello("Kent Beck")
 say_hello
 require_relative "say_hello.rb"
